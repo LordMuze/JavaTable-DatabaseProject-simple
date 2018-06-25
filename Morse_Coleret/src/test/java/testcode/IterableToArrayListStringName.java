e924e961e5a1ebf6ad3592d7faa253ee75c81e0f
lordm <lordm@172.22.35.230>
2018-06-25 12:47:03 +0800

    .

--- Morse_Coleret/src/test/java/testcode/IterableToArrayListStringName.java
+++ Morse_Coleret/src/test/java/testcode/IterableToArrayListStringName.java
@@ -2,4 +2,7 @@ package testcode;
 
 public class IterableToArrayListStringName {
 	private IterableToArrayList iterable = new IterableToArrayList();
+	public void arrayListStringArray() {
+		
+	}
 }
